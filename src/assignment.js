a5a2de23da175a316204c7bf620a0339331d9fd5
danielsolution <93793434+danielsolution@users.noreply.github.com>
2022-07-10 16:52:01 +0000

    attempt -2

--- src/assignment.js
+++ src/assignment.js
@@ -32,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count =0;
+    for (let i= 0; i < arrayOfNumbers.length; i++) 
+    if (arrayOfNumbers[i]%2===0) {
+        count++
+        
+    }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
