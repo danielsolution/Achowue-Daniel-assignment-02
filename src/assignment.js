ba958ec4be7a392fc39762b2a8e6d0014c54fe5c
danielsolution <93793434+danielsolution@users.noreply.github.com>
2022-07-10 16:26:01 +0000

    attempt -1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum=0;
+    for(number of arrayOfNumbers){
+        sum += number
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
